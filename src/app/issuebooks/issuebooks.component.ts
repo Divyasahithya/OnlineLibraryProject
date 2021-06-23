f971f2824d17b484dc3da89a523e5779c1c35059
divyasahithya2 <divyasahithya2@gmail.com>
2021-06-23 10:33:59 +0530

    return and delete updates

--- src/app/issuebooks/issuebooks.component.ts
+++ src/app/issuebooks/issuebooks.component.ts
@@ -16,7 +16,7 @@ HttpErrorResponse
 export class IssuebooksComponent implements OnInit {
  book:Viewbooks[]=[];
   constructor(private location:Location,private booksissued:IssueserviceService,private bookservice:ViewbookserviceService) { }
-  addissuebook:issuebook = new issuebook("",0,"","","no");
+  addissuebook:issuebook = new issuebook(0,"",0,"","","no");
   message:any;
   issuebook = new FormGroup(
     {
@@ -37,7 +37,7 @@ export class IssuebooksComponent implements OnInit {
     return this.issuebook.get('name')?.value
   }
   get mobile(){ 
-    return this.issuebook.get('publisher')?.value
+    return this.issuebook.get('mobile')?.value
   }
 
   ngOnInit(): void {
