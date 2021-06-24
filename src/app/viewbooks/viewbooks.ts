1cb8358ab385e177334483164d401641a356a347
sudharsan <sudharsan1136@gmail.com>
2021-06-24 17:15:12 +0530

    last update

--- src/app/viewbooks/viewbooks.ts
+++ src/app/viewbooks/viewbooks.ts
@@ -1,8 +1,4 @@
 export class Viewbooks {
-    
-     
-        constructor(public callno:string,public name:string,public author:string,public publisher:string,
-            public quantity:number,public issued:number,public added_date:string){}
-  
-
+  constructor(public callno:string,public name:string,public author:string,public publisher:string,
+              public quantity:number,public issued:number,public added_date:string){}
 }
